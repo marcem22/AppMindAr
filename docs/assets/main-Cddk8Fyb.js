fe018e07a5ca899cf3fa39683b8295e10ac6ac6a
Brizuela-Mara94 <brizuelamara94@iinfo.unsj.edu.ar>
2026-07-31 13:38:23 -0300

    deploy

---
+++ docs/assets/main-Cddk8Fyb.js
@@ -0,0 +1,1 @@
+import"./tutorial-BALdaNut.js";import{tutorial as t}from"./tutorial-DSW_G304.js";document.getElementById("btnStartTutorial").addEventListener("click",()=>{t.startTutorial()});t.checkAndInit("index");
--- docs/assets/main-DmrswiV9.js
+++
@@ -1,1 +0,0 @@
-import"./tutorial-BALdaNut.js";import{tutorial as t}from"./tutorial-LzeCRVcj.js";document.getElementById("btnStartTutorial").addEventListener("click",()=>{t.startTutorial()});t.checkAndInit("index");
